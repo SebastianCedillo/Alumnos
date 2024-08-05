ec4585730b757c83f6d7f4194076ee7e210c1d96
sebas <sebas@DESKTOP-0OS6OUS>
2024-08-04 20:26:37 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Alumnos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtp_Fecha = new System.Windows.Forms.DateTimePicker();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.btn_Eliminar = new System.Windows.Forms.Button();
+            this.txt_Telefono = new System.Windows.Forms.TextBox();
+            this.txt_Nombres = new System.Windows.Forms.TextBox();
+            this.txt_Apellidos = new System.Windows.Forms.TextBox();
+            this.txt_Direccion = new System.Windows.Forms.TextBox();
+            this.txt_cedula = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Agregar = new System.Windows.Forms.Button();
+            this.lst_Alumnos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // dtp_Fecha
+            // 
+            this.dtp_Fecha.Location = new System.Drawing.Point(189, 273);
+            this.dtp_Fecha.Name = "dtp_Fecha";
+            this.dtp_Fecha.Size = new System.Drawing.Size(200, 20);
+            this.dtp_Fecha.TabIndex = 34;
+            this.dtp_Fecha.ValueChanged += new System.EventHandler(this.dtp_Fecha_ValueChanged);
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Location = new System.Drawing.Point(189, 327);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(88, 41);
+            this.btn_Cancelar.TabIndex = 33;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
+            // 
+            // btn_Eliminar
+            // 
+            this.btn_Eliminar.Location = new System.Drawing.Point(293, 327);
+            this.btn_Eliminar.Name = "btn_Eliminar";
+            this.btn_Eliminar.Size = new System.Drawing.Size(88, 41);
+            this.btn_Eliminar.TabIndex = 32;
+            this.btn_Eliminar.Text = "Eliminar";
+            this.btn_Eliminar.UseVisualStyleBackColor = true;
+            this.btn_Eliminar.Click += new System.EventHandler(this.btn_Eliminar_Click);
+            // 
+            // txt_Telefono
+            // 
+            this.txt_Telefono.Location = new System.Drawing.Point(189, 235);
+            this.txt_Telefono.Name = "txt_Telefono";
+            this.txt_Telefono.Size = new System.Drawing.Size(200, 20);
+            this.txt_Telefono.TabIndex = 31;
+            this.txt_Telefono.TextChanged += new System.EventHandler(this.txt_Telefono_TextChanged);
+            // 
+            // txt_Nombres
+            // 
+            this.txt_Nombres.Location = new System.Drawing.Point(189, 119);
+            this.txt_Nombres.Name = "txt_Nombres";
+            this.txt_Nombres.Size = new System.Drawing.Size(200, 20);
+            this.txt_Nombres.TabIndex = 30;
+            this.txt_Nombres.TextChanged += new System.EventHandler(this.txt_Nombres_TextChanged);
+            // 
+            // txt_Apellidos
+            // 
+            this.txt_Apellidos.Location = new System.Drawing.Point(189, 159);
+            this.txt_Apellidos.Name = "txt_Apellidos";
+            this.txt_Apellidos.Size = new System.Drawing.Size(200, 20);
+            this.txt_Apellidos.TabIndex = 29;
+            this.txt_Apellidos.TextChanged += new System.EventHandler(this.txt_Apellidos_TextChanged);
+            // 
+            // txt_Direccion
+            // 
+            this.txt_Direccion.Location = new System.Drawing.Point(189, 198);
+            this.txt_Direccion.Name = "txt_Direccion";
+            this.txt_Direccion.Size = new System.Drawing.Size(200, 20);
+            this.txt_Direccion.TabIndex = 28;
+            this.txt_Direccion.TextChanged += new System.EventHandler(this.txt_Direccion_TextChanged);
+            // 
+            // txt_cedula
+            // 
+            this.txt_cedula.Location = new System.Drawing.Point(189, 77);
+            this.txt_cedula.Name = "txt_cedula";
+            this.txt_cedula.Size = new System.Drawing.Size(200, 20);
+            this.txt_cedula.TabIndex = 27;
+            this.txt_cedula.TextChanged += new System.EventHandler(this.txt_cedula_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(129, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Cedula\r\n";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Nombres";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(120, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Apellidos";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Direccion";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Telefono";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Fecha Nacimiento";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Ingreso de Alumnos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_Agregar
+            // 
+            this.btn_Agregar.Location = new System.Drawing.Point(79, 327);
+            this.btn_Agregar.Name = "btn_Agregar";
+            this.btn_Agregar.Size = new System.Drawing.Size(88, 41);
+            this.btn_Agregar.TabIndex = 19;
+            this.btn_Agregar.Text = "Agregar";
+            this.btn_Agregar.UseVisualStyleBackColor = true;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
+            // 
+            // lst_Alumnos
+            // 
+            this.lst_Alumnos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst_Alumnos.FormattingEnabled = true;
+            this.lst_Alumnos.ItemHeight = 24;
+            this.lst_Alumnos.Location = new System.Drawing.Point(471, 56);
+            this.lst_Alumnos.Name = "lst_Alumnos";
+            this.lst_Alumnos.Size = new System.Drawing.Size(330, 436);
+            this.lst_Alumnos.TabIndex = 18;
+            this.lst_Alumnos.SelectedIndexChanged += new System.EventHandler(this.lst_Alumnos_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 577);
+            this.Controls.Add(this.dtp_Fecha);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Eliminar);
+            this.Controls.Add(this.txt_Telefono);
+            this.Controls.Add(this.txt_Nombres);
+            this.Controls.Add(this.txt_Apellidos);
+            this.Controls.Add(this.txt_Direccion);
+            this.Controls.Add(this.txt_cedula);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Agregar);
+            this.Controls.Add(this.lst_Alumnos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtp_Fecha;
+        private System.Windows.Forms.Button btn_Cancelar;
+        private System.Windows.Forms.Button btn_Eliminar;
+        private System.Windows.Forms.TextBox txt_Telefono;
+        private System.Windows.Forms.TextBox txt_Nombres;
+        private System.Windows.Forms.TextBox txt_Apellidos;
+        private System.Windows.Forms.TextBox txt_Direccion;
+        private System.Windows.Forms.TextBox txt_cedula;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Agregar;
+        private System.Windows.Forms.ListBox lst_Alumnos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alumnos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lst_Alumnos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Eliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Telefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Nombres_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Apellidos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Direccion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_cedula_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Agregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtp_Fecha_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
